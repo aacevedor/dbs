eec97ee4d3dbdba540e2d9d4edc78b932ccee12b
aacevedoday <aacevedo@dayscriopt.com>
2017-03-12 19:37:16 -0700

    update

--- dayscript_system_servers/dss/variables.py
+++ dayscript_system_servers/dss/variables.py
@@ -1,5 +1,2 @@
 
-ionic = {'url':'https://api.ionic.io/push/notifications'}
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhNTdlNDEyNi03MDNmLTQ1NDctODA1Ny0zYTI0ODkzNDEyZGMifQ.Kf7j23mS7pwu-wr24kv5AN2DKjhnS20I4ATDdLFjLSU"
-
 
